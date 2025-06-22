6bded3b8b8cd83f8866287341cbdf2de648781ec
velton81 <153524734+velton81@users.noreply.github.com>
2025-06-22 16:40:14 +0700

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/Footer.css";
+
+function Footer() {
+    return (
+        <footer className="site-footer">
+            <div className="footer-content">
+                <div className="footer-copyright">
+                    © 2024 seruhipipo.com - All rights reserved
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
